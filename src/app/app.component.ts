9f53c7a42ef9d2acd89ecd14b4ba2b563f99d5df
g.coletta <giali.coletta@fullstackagency.it>
2023-05-01 11:30:07 +0200

    tailwind/daisyui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { invoke } from "@tauri-apps/api/tauri";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styles: [],
 })
 export class AppComponent {
   greetingMessage = "";
